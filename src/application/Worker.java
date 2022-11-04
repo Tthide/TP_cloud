6b1208d1fa08f6d3dae3a566d716dd4529112ee5
Thibault Borde <bordethibault@gmail.com>
2022-11-04 17:34:03 +0100

    Ajout recherche d'item dans le SQS + ajout suppression des fichiers temp dans le bucket worker

--- src/application/Worker.java
+++ src/application/Worker.java
@@ -1,55 +1,104 @@
 package application;
 
 import application.GetMessage;
+
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.util.IOUtils;
+
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.sqs.model.Message;
 
 public class Worker {
-	
+
 	public static String GetObjectNameFromQueue(String QueueUrl, Region region) {
-		
-		String[] args =  {QueueUrl};
-		List<Message> messages = GetMessage.SnsGetMessage(QueueUrl ,region);
+
+		String[] args = { QueueUrl };
+		List<Message> messages = GetMessage.SnsGetMessage(QueueUrl, region);
 		String String_message = messages.get(0).body().toString();
-		
-		
+
 		return String_message;
 	}
-	
-	
-	
-	
-	public void ReadCSV(String file_name, String bucket_name) {
-		
-		//Get file
-		Region region = Region.US_EAST_1;
 
-	    S3Client s3 = S3Client.builder().region(region).build();
+	public static String ReadCSV(String bucketName, String date){
 
-	    GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket(bucket_name)
-                .key(file_name)
-                .build();
+		// Get file
+		Regions region = Regions.US_EAST_1;
+		S3Object fullObject = null;
+		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withRegion(region).build();
 
-        s3.getObject(getObjectRequest);
-        // 
 		
-	}
-	
-	public static void main(String[] args) { // input : ( String bucketName, String filePath)
-		Region region = Region.US_EAST_1;
-		String QueueUrl="https://sqs.us-east-1.amazonaws.com/446929553961/messaging-app-queue";
-		
-		String NextItemInQueue = GetObjectNameFromQueue(QueueUrl,region);
-		System.out.println(NextItemInQueue);
+		fullObject = s3Client.getObject(new GetObjectRequest(bucketName, date));
+		//
+
+		// Convert Input into String
+		try (S3ObjectInputStream data = fullObject.getObjectContent()) {
+			return IOUtils.toString(data);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+		//
 	}
 
+	public static void main(String[] args) { // input : none
+		Region region = Region.US_EAST_1;
+		String QueueUrl = "https://sqs.us-east-1.amazonaws.com/446929553961/Tp_cloud_Queue";
+		
+
+		//get object in queue
+		String NextItemInQueue = GetObjectNameFromQueue(QueueUrl, region);
+		
+		String[] items = NextItemInQueue.split(";");
+		String bucketNameWorker = items[0];
+		String filename=items[1];
+		System.out.println(bucketNameWorker);
+		System.out.println(filename);
+		
+		try {
+			
+			String csv_data=ReadCSV(bucketNameWorker,filename);
+			
+			System.out.print(csv_data);
+			
+
+			}
+			catch (S3Exception e) {
+	            System.err.println(e.awsErrorDetails().errorMessage());
+	            System.exit(1);
+			}
+
+		//deleting temporary csv file in worker bucket
+		
+		S3deletefile.deleteFile(bucketNameWorker, filename);
+		
+		
+		// code de steve
+
+		// Uploading file to consolidator bucket
+
+		String bucketNameConsolidator = "consolidatorbucket01";
+
+		try {
+			S3uploadfile.main(new String[] { bucketNameConsolidator, /* remplacer par le nom de la variable */ "15" });
+			SendMessage.main(new String[] { QueueUrl, bucketNameConsolidator, "15" });
+
+		} catch (S3Exception e) {
+			// System.err.println(e.awsErrorDetails().errorMessage());
+			// System.exit(1);
+
+		}
+	}
 
 }
